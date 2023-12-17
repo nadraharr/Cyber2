a72516d4903d9409da88fbcd2ab0bbf6c1fb8703
nadraharr <nadraharr@gmail.com>
2023-12-17 09:33:15 +0200

    graphql_controller, query_type

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+module Types
+  class QueryType < Types::BaseObject
+    field :node, Types::NodeType, null: true, description: 'Fetches an object given its ID.' do
+      argument :id, ID, required: true, description: 'ID of the object.'
+    end
+
+    def node(id:)
+      context.schema.object_from_id(id, context)
+    end
+
+    field :nodes, [Types::NodeType, { null: true }], null: true,
+                                                     description: 'Fetches a list of objects given a list of IDs.' do
+      argument :ids, [ID], required: true, description: 'IDs of the objects.'
+    end
+
+    def nodes(ids:)
+      ids.map { |id| context.schema.object_from_id(id, context) }
+    end
+
+    
+    field :repos, String, null: false do
+      argument :userLogin, String, required: true
+    end
+    def repos(userLogin:)
+      token = ENV['GITHUB_TOKEN']
+      connection = Faraday.new(url: "https://api.github.com/users/#{userLogin}/repos") do |faraday|
+      faraday.headers['Content-Type'] = 'application/json'
+      faraday.headers['Authorization'] = "Bearer #{token}"
+      faraday.adapter Faraday.default_adapter
+    end
+      response = connection.get
+      result = JSON.parse(response.body)
+    end
+
+    field :name, String, null: false do
+      argument :userLogin, String, required: true
+    end
+    def name(userLogin:)
+      token = ENV['GITHUB_TOKEN']
+      connection = Faraday.new(url: "https://api.github.com/users/#{userLogin}") do |faraday|
+      faraday.headers['Content-Type'] = 'application/json'
+      faraday.headers['Authorization'] = "Bearer #{token}"
+      faraday.adapter Faraday.default_adapter
+    end
+      response = connection.get
+      result = JSON.parse(response.body)
+    end
+
+  end
+end
